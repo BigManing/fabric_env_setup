3f990f6f83a7537a6eb7815613358a81320e1cde
BigManing <lingshui2008@qq.com>
2018-11-22 10:32:45 +0800

    添加git安装

--- centos7.sh
+++ centos7.sh
@@ -27,7 +27,7 @@ installPip(){
 installDocker(){
     printOperation "installDocker"
     sudo yum  -y  remove docker  docker-common docker-selinux docker-engine
-    #sudo yum  -y  install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum  -y  install -y yum-utils device-mapper-persistent-data lvm2
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     yum list docker-ce --showduplicates | sort -r
     sudo  yum  -y  install docker-ce
@@ -77,7 +77,7 @@ installGo(){
 }
  
 echo "---------------------开始安装基础环境----------------------------"
-
+installGit
 installPip
 installDocker
 installDockerCompose
